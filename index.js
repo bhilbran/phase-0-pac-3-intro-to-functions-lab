e7d0a593b9f165907bb154e2876a64e9849a913c
brooke <bhilbran@asu.edu>
2023-05-28 08:37:51 -0700

    complete

---
+++ index.js
@@ -0,0 +1,39 @@
+function shout(string) {
+    // todo
+  }
+
+  function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+    // todo
+  }
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  function logShout(string) {
+    // todo
+  }
+  function logShout(string) {
+    console.log('HELLO') 
+  }
+
+function logWhisper(string) {
+  // todo
+} function logWhisper(string) {
+    console.log('hello') 
+  }
+
+function sayHiToHeadphonedRoommate(string) {
+    switch (string) { 
+        case string.toLowerCase():
+            return "I can't hear you!";
+            break; 
+        case string.toUpperCase():
+            return "YES INDEED!";
+            break;
+        case "Let's have dinner together!":
+            return "I would love to!";
+            break;
+    }
+  }
